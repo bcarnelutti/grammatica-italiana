3552b16cd15d8a0566d48ca383bccfb498b7318d
Beatrice Carnelutti <beatrice@Mac.localdomain>
2026-01-22 21:32:31 -0500

    chore: update vite config for local server

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
   base: command === 'build' ? '/grammatica-italiana/' : '/',
+  server: {
+    host: '127.0.0.1',
+    port: 5177,
+    strictPort: true,
+  },
   plugins: [
     react(),
     tailwindcss(),
